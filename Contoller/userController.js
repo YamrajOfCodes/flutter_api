7d3a5b5fe45c72d54847b958aa7602c88652836d
YamrajOfcodes <kundanpatil0111@.gmail.com>
2025-02-27 12:13:02 +0530

    sds

--- Contoller/userController.js
+++ Contoller/userController.js
@@ -4,24 +4,23 @@ const userDb = require("../Model/userSchema");
 const adduser = async(req,res)=>{
     try {
         const {pname,pphone,page} = req.body;
-
-          if(!pname || !pphone || !page){
-            return res.status(400).json({error:"all fields are required"});
-          }
-          
-          const user = new userDb({
-            username:pname,
-            phone:pphone,
-            age:page,
-          })
+       
         
-          await user.save();
+
+         
+        //   const user = new userDb({
+        //     username:pname,
+        //     phone:pphone,
+        //     age:page,
+        //   })
+        
+        //   await user.save();
             
         
             res.status(200).send({
                 "statuscode":200,
                 "message":"the data has sent",
-                "data":user
+                "data":req.body
             })
     } catch (error) {
         
