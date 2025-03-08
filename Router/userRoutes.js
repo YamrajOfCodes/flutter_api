b52185fee3d171673e0e2b16d4133dea06911857
YamrajOfcodes <kundanpatil0111@.gmail.com>
2025-03-08 10:37:48 +0530

    change in route

--- Router/userRoutes.js
+++ Router/userRoutes.js
@@ -5,7 +5,7 @@ router.post("/adduser",userController.adduser);
 router.get("/getuser",userController.getuser);
 router.get("/deleteuser",userController.deleteuser);
 
-router.get("/adminlogin",userController.adminLogin);
+router.post("/adminlogin",userController.adminLogin);
 
 
 
